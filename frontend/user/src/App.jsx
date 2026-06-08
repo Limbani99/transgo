5deb03b4dbf412d882157aa90eee68c501f6b396
mannlimbani <Patelmann2424@gmail.com>
2026-06-08 13:16:28 +0530

    pages

--- frontend/user/src/App.jsx
+++ frontend/user/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div>App</div>
+    <>
+      <routes>
+        <Route path='/' element={<Layout />}>
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Home />} />
+        </Route>
+      </routes>
+    </>
   )
 }
 
--- frontend/user/src/main.jsx
+++ frontend/user/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import browserRouter from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <browserRouter>
+      <App />
+    </browserRouter>
   </StrictMode>,
 )
