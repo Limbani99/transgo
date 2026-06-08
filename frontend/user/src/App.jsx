c226590f427316ebebdd871c3efd7717487dc777
mannlimbani <Patelmann2424@gmail.com>
2026-06-08 14:33:22 +0530

    reg login

--- frontend/user/src/App.jsx
+++ frontend/user/src/App.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
-import { Route,Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Layout from './layouts/Layout'
 import Register from './auth/Register'
+import Login from './auth/Login'
 import Home from './pages/Home'
+
 function App() {
   return (
     <>
       <Routes>
         <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
         </Route>
         <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<login />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   )
